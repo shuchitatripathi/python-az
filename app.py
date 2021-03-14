517a586548124a091f0b465995c1a52f14d652f8
shuchitatripathi <42193853+shuchitatripathi@users.noreply.github.com>
2021-03-15 02:54:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+import os
+import socket
+
+app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 5000))
+
+@app.route("/")
+def hello():
+    html = "<body style=\"background-color:lightgray;\">" \
+           "<h2 style=\"background-color:lightgray;\" >Hello from Python!</h2>" \
+           "<h3>AppVersion : 1.0</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "</body>"
+
+    return html.format(hostname=socket.gethostname())
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port, debug=True)
