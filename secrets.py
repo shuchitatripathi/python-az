843d012ab320f854dabd979f0e910350e17cce6d
shuchitatripathi <42193853+shuchitatripathi@users.noreply.github.com>
2021-03-18 17:19:34 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -5,26 +5,25 @@ from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 def getSecret():
+    temp_json = {}
+    rt_json = {}
+    final_json = {}
+    
     keyVaultName = os.environ["KEY_VAULT_NAME"]
     KVUri = f"https://{keyVaultName}.vault.azure.net"
 
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=KVUri, credential=credential)
 
-    retrieved_secret = client.get_secret("kvtest1")
+    vnet_secret = client.get_secret("vnet")
+    vnet = json.loads(vnet_secret.value)
     timeStampFormatted = getFormattedTimestamp()
-    temp_json = {}
-    final_json = {}
-    rt_json = {}
-    vnet = {"id" : "someid", "addressPrefix" : ["someAP", "new AP"]}
 
     for key,value in vnet.items():
         temp_json[key] = value
         final_json["vnet"] = temp_json
 
     final_json["timestamp"] = timeStampFormatted
-    final_json["kvtest1"] = retrieved_secret.value
-    final_json["test"] = "test_value"
     final_json = json.dumps(final_json, indent=4)
     return final_json
 
